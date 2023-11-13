07dd3685b09451b05772ab6800510d0f2b498009
damd2212 <53495267+damd2212@users.noreply.github.com>
2023-11-13 11:59:31 -0500

    Eliminado acceso a login :+1:

--- Admin/src/app/app-routing.module.ts
+++ Admin/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { AuthlayoutComponent } from './authlayout/authlayout.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard]  },
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
   { path: 'auth', component: AuthlayoutComponent, loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
   { path: 'pages',component: AuthlayoutComponent, loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule)},
 ];
